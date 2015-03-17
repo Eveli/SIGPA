c99053d4facb4d8738dfe9537b7a7294c10b8ad0
DeliaAraque <delia.araque11@gmail.com>
2015-03-17 21:26:27 +0000

    Modificando gitignore

--- lib/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$sigpa=pg_connect("host=localhost dbname=SIGPA user=postgres password=root");
-
-	function historial($sigpa, $ci, $d)
-	{
-		pg_query($sigpa, "insert into historial(ci, d) values('$ci', '".pg_escape_string($d)."')");
-	}
-?>
